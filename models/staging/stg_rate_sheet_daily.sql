82d9a0c4408925916c63dc864e06c8c41ea21e1a
Aaron Phethean <aphethean@matatika.com>
2025-03-17 15:39:09 +0000

    Add an optional source that provides a simple way to supply the daily rates if they are not available in Snowflake because the account is purchased throught a 3rd party e.g. AWS

--- models/staging/stg_rate_sheet_daily.sql
+++ models/staging/stg_rate_sheet_daily.sql
@@ -14,4 +14,20 @@ select
     effective_rate,
     service_type
 from {{ source('snowflake_organization_usage', 'rate_sheet_daily') }}
+{% if var('raw_config_rate_sheet_daily_enabled', false) %}
+union all
+select
+    date,
+    organization_name,
+    contract_number,
+    account_name,
+    account_locator,
+    region,
+    service_level,
+    usage_type,
+    currency,
+    effective_rate,
+    service_type
+from {{ source('raw_config', 'rate_sheet_daily') }}
+{% endif %}
 order by date
